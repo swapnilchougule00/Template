af7197e79936d4e874a8e8ba3a53c1679350cb0e
swapnilchougule00 <swapnilchougule00@gmail.com>
2024-08-08 00:46:25 +0530

    Initial commit: set up project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./apiSlice/auth/authApi";
-import { dashboardApi } from "./apiSlice/Dashboard/dashboardApi";
-import { profileApi } from "./apiSlice/profileFIll/profileApi";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
-    [profileApi.reducerPath]: profileApi.reducer,
-    [dashboardApi.reducerPath]: dashboardApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      authApi.middleware,
-      profileApi.middleware,
-      dashboardApi.middleware
-    ),
+    getDefaultMiddleware().concat(authApi.middleware),
 });
